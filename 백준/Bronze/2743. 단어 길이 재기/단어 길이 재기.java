16ebe846927763f7a4145b6b59a1460d28d48239
pleadsori <plump74@gmail.com>
2025-07-17 15:30:25 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String N = sc.next();		//문자열 받기
+	
+	System.out.println(N.length());
+	}
+}
